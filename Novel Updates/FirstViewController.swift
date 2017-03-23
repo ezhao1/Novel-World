0b5db5a531121603d54c2ed36d16d258dd2a5830
Ethan Zhao <ethanzhao@Ethans-MacBook-Pro.local>
2017-03-23 01:18:02 -0400

    Initial Commit

---
+++ Novel Updates/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Novel Updates
+//
+//  Created by Ethan Zhao on 3/23/17.
+//  Copyright © 2017 Ethan Zhao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
